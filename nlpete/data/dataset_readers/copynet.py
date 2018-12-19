f8e32d7980dfca586aa312fefc372c4ad0ded3fb
epwalsh <epwalsh10@gmail.com>
2018-12-19 15:07:57 -0800

    switch to more efficient field representations

--- nlpete/data/dataset_readers/copynet.py
+++ nlpete/data/dataset_readers/copynet.py
@@ -84,25 +84,12 @@ class CopyNetDatasetReader(DatasetReader):
                 yield self.text_to_instance(source_sequence, target_sequence)
 
     @staticmethod
-    def _create_target_to_source_array(tokenized_source: List[Token],
-                                       tokenized_target: List[Token]) -> np.array:
-        target_to_source_array: List[List[int]] = []
-        for target_token in tokenized_target[1:-1]:
-            source_index_list: List[int] = [int(target_token.text.lower() == source_token.text.lower())
-                                            for source_token in tokenized_source[1:-1]]
-            target_to_source_array.append(source_index_list)
-        target_to_source_array.insert(0, [0] * len(tokenized_source[1:-1]))
-        target_to_source_array.append([0] * len(tokenized_source[1:-1]))
-        return np.array(target_to_source_array)
-
-    @staticmethod
-    def _create_source_to_source_array(tokenized_source: List[Token]) -> np.array:
-        out_array: List[List[int]] = []
-        for token in tokenized_source[1:-1]:
-            array_slice: List[int] = [int(token.text.lower() == other.text.lower())
-                                      for other in tokenized_source[1:-1]]
-            out_array.append(array_slice)
-        return np.array(out_array)
+    def _tokens_to_ids(tokens: List[Token]) -> List[int]:
+        ids: Dict[str, int] = {}
+        out: List[int] = []
+        for token in tokens:
+            out.append(ids.setdefault(token.text.lower(), len(ids)))
+        return out
 
     @overrides
     def text_to_instance(self, source_string: str, target_string: str = None) -> Instance:  # type: ignore
@@ -123,10 +110,14 @@ class CopyNetDatasetReader(DatasetReader):
                including the `START_SYMBOL` and `END_SYMBOL`.
                This will result in a tensor of shape `(batch_size, source_length)`.
 
-            - `source_to_source`: an `ArrayField` that holds spare binary array of shape
-              `(trimmed_source_length, trimmed_source_length)` that indicates which
-              source tokens match each other. This will result in a tensor of shape
-              `(batch_size, trimmed_source_length, trimmed_source_length)`.
+            - `source_token_ids`: an `ArrayField` of size `(batch_size, trimmed_source_length)`
+              that contains an ID for each token in the source sentence. Tokens that
+              match at the lowercase level will share the same ID. If `target_tokens`
+              is passed as well, these IDs will also correspond to the `target_token_ids`
+              field, i.e. any tokens that match at the lowercase level in both
+              the source and target sentences will share the same ID. Note that these IDs
+              have no correlation with the token indices from the corresponding
+              vocabulary namespaces.
 
             - `source_to_target`: a `CopyMapField` that keeps track of the index
               of the target token that matches each token in the source sentence.
@@ -142,21 +133,19 @@ class CopyNetDatasetReader(DatasetReader):
               including the `START_SYMBOL` and `END_SYMBOL`. This will result in
               a tensor of shape `(batch_size, target_length)`.
 
-            - `target_to_source`: an `ArrayField` containing a sparse binary array of shape
-              `(target_length, trimmed_source_length)` that indicates which source
-              tokens match each target token. This will result in a tensor of shape
-              `(batch_size, target_length, trimmed_source_length)`.
+            - `target_token_ids`: an `ArrayField` of size `(batch_size, target_length)`.
+              This is calculated in the same way as `source_token_ids`.
 
         Notes
         -----
         By `source_length` we are referring to the number of tokens in the source
         sentence including the `START_SYMBOL` and `END_SYMBOL`, while
         `trimmed_source_length` refers to the number of tokens in the source sentence
-        excluding the `START_SYMBOL` and `END_SYMBOL`, i.e.
+        *excluding* the `START_SYMBOL` and `END_SYMBOL`, i.e.
         `trimmed_source_length = source_length - 2`.
 
-        Similarly, `target_length` is the number of tokens in the target sentence
-        including the `START_SYMBOL` and `END_SYMBOL`.
+        On the other hand, `target_length` is the number of tokens in the target sentence
+        *including* the `START_SYMBOL` and `END_SYMBOL`.
 
         In the context where there is a `batch_size` dimension, the above refer
         to the maximum of their individual values across the batch.
@@ -167,15 +156,6 @@ class CopyNetDatasetReader(DatasetReader):
         tokenized_source.append(Token(END_SYMBOL))
         source_field = TextField(tokenized_source, self._source_token_indexers)
 
-        # For token in the source sentence, we store a sparse array containing
-        # indicators for each other source token that matches. This gives us
-        # a matrix of shape `(trimmed_source_length, trimmed_source_length)`
-        # where the (i,j)th entry is a 1 if the ith token matches the jth token.
-        # Here `trimmed_source_length` = number of tokens in `tokenized_source`
-        # excluded the `START_SYMBOL` and `END_SYMBOL`.
-        source_to_source_array = self._create_source_to_source_array(tokenized_source)
-        source_to_source_field = ArrayField(source_to_source_array)
-
         # For each token in the source sentence, we keep track of the matching token
         # in the target sentence (which will be the OOV symbol if there is no match).
         source_to_target_field = CopyMapField(tokenized_source[1:-1], self._target_namespace)
@@ -183,7 +163,6 @@ class CopyNetDatasetReader(DatasetReader):
         meta_fields = {"source_tokens": [x.text for x in tokenized_source[1:-1]]}
         fields_dict = {
                 "source_tokens": source_field,
-                "source_to_source": source_to_source_field,
                 "source_to_target": source_to_target_field,
         }
 
@@ -193,16 +172,17 @@ class CopyNetDatasetReader(DatasetReader):
             tokenized_target.append(Token(END_SYMBOL))
             target_field = TextField(tokenized_target, self._target_token_indexers)
 
-            # For each token in the target sentence, we keep track of the index
-            # of every token in the source sentence that matches.
-            target_to_source_array = self._create_target_to_source_array(tokenized_source,
-                                                                         tokenized_target)
-            # shape: (target_length, trimmed_source_length)
-            target_to_source_field = ArrayField(target_to_source_array)
-
             fields_dict["target_tokens"] = target_field
-            fields_dict["target_to_source"] = target_to_source_field
             meta_fields["target_tokens"] = [y.text for y in tokenized_target[1:-1]]
+            source_and_target_token_ids = self._tokens_to_ids(tokenized_source[1:-1] +
+                                                              tokenized_target)
+            source_token_ids = source_and_target_token_ids[:len(tokenized_source)-2]
+            fields_dict["source_token_ids"] = ArrayField(np.array(source_token_ids))
+            target_token_ids = source_and_target_token_ids[len(tokenized_source)-2:]
+            fields_dict["target_token_ids"] = ArrayField(np.array(target_token_ids))
+        else:
+            source_token_ids = self._tokens_to_ids(tokenized_source[1:-1])
+            fields_dict["source_token_ids"] = ArrayField(np.array(source_token_ids))
 
         fields_dict["metadata"] = MetadataField(meta_fields)
 
--- nlpete/tests/data/dataset_readers/nl2bash_test.py
+++ nlpete/tests/data/dataset_readers/nl2bash_test.py
@@ -21,23 +21,6 @@ class TestNL2BashReader(AllenNlpTestCase):
         assert [t.text for t in fields["target_tokens"].tokens] == \
             ["@start@", "bunzip", "2", " ", "file", ".", "bz", "2", "@end@"]
 
-    def test_target_to_source(self):
-        target_to_source = self.instances[2].fields["target_to_source"]
-
-        # shape should be (target_length, source_length - 2)
-        assert target_to_source.array.shape == (9, 9)
-
-        check = np.array([[0, 0, 0, 0, 0, 0, 0, 0, 0],  # @START@
-                          [0, 0, 0, 0, 0, 0, 0, 0, 0],  # bunzip
-                          [0, 0, 0, 0, 0, 1, 0, 0, 0],  # 2
-                          [0, 1, 0, 1, 0, 0, 1, 0, 0],  # \s
-                          [0, 0, 0, 0, 0, 0, 0, 1, 0],  # file
-                          [0, 0, 0, 0, 0, 0, 0, 0, 1],  # .
-                          [0, 0, 0, 0, 1, 0, 0, 0, 0],  # bz
-                          [0, 0, 0, 0, 0, 1, 0, 0, 0],  # 2
-                          [0, 0, 0, 0, 0, 0, 0, 0, 0]]) # @END@
-        np.testing.assert_equal(target_to_source.array, check)
-
     def test_preprocess_target(self):
         # pylint: disable=protected-access
         tgt_str = "sudo find ."
